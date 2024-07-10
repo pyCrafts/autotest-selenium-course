e40fee6db5b161c7ed5a6e9ea743f67a740a9f05
Oleg <oleg.brest.1997@gmail.com>
2024-07-11 02:55:39 +0300

    need review

--- test_product_page.py
+++ test_product_page.py
@@ -6,8 +6,34 @@ import time
 
 
 @pytest.mark.need_review
-class TestGuestAndUserProductPage:
-    def test_user_can_add_product_to_basket(self, browser):
+def test_guest_can_add_product_to_basket(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_basket_product()
+
+
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.check_products_in_basket()
+
+
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page
+
+
+@pytest.mark.need_review
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
         email = str(time.time()) + "@fakemail.org"
         password = "Lolpass123!"
         link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
@@ -16,21 +42,8 @@ class TestGuestAndUserProductPage:
         page.register_new_user(email, password)
         page.should_be_authorized_user()
 
-    def test_guest_can_add_product_to_basket(self, browser):
-        link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_add_basket_product()
-
-    def test_guest_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = BasketPage(browser, link)
-        page.open()
-        page.go_to_basket_page()
-        page.check_products_in_basket()
-
-    def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.go_to_login_page
