3268c6a7c654ae971bab5670730dbd7e58f069e4
Oleg <oleg.brest.1997@gmail.com>
2024-07-11 03:00:53 +0300

    need review

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,7 @@ import time
 
 @pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
-    link = "https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "https://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_basket_product()
@@ -15,7 +15,7 @@ def test_guest_can_add_product_to_basket(browser):
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -24,7 +24,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
 
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page
@@ -36,7 +36,7 @@ class TestUserAddToBasketFromProductPage:
     def setup(self, browser):
         email = str(time.time()) + "@fakemail.org"
         password = "Lolpass123!"
-        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        link = "http://selenium1py.pythonanywhere.com/accounts/login/"
         page = LoginPage(browser, link)
         page.open()
         page.register_new_user(email, password)
